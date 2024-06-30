c58fa0059411b5acd9f8cfa84aa0b8a4d528567c
Deeptanu Bhatta <deeptanubhatta2002@gmail.com>
2024-06-30 20:18:49 +0530

    added new files

---
+++ Graphs/g1.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void addEdge(vector<int>adj[],int u, int v){
+
+   adj[u].push_back(v);
+   adj[v].push_back(u);
+        
+}
+
+void printGraph(vector<int>adj[],int v){
+    for(int i=0;i<v;i++){
+        for(auto x :adj[i]){
+            cout<<x<<" ";
+        }
+
+        cout<<endl;
+    }
+
+}
+
+int main(){
+    int v=4;
+
+    vector<int>adj[v];
+    addEdge(adj,0,1);
+    addEdge(adj,0,2);
+    addEdge(adj,1,2);
+    addEdge(adj,2,3);
+
+    printGraph(adj,v);
+
+
+
+    return 0;
+}
